d0e2ae8cdf8c8733eb87d4cd0d99595bda9d3e84
Jackie Zheng <jackyzheng.zheng@gmail.com>
2024-03-04 21:25:32 -0800

    chore: reduce led

--- frontend/src/app/_components/meetTheTeam/ledSign/ledLetter.tsx
+++ frontend/src/app/_components/meetTheTeam/ledSign/ledLetter.tsx
@@ -23,11 +23,11 @@ const flicker = keyframes`
 const turn_on_animation = keyframes`
   19.999%, 22%, 62.999%, 64%, 64.999%, 70%, to {
     opacity: .99;
-    text-shadow: 0 0 1rem #fff, 0 0 2rem var(--neon-text-shadow), 0 0 3rem var(--neon-text-shadow), 0 0 4rem var(--neon-text-shadow), 0 0 5rem var(--neon-text-edge-color), 0 0 6rem var(--neon-text-edge-color), 0 0 7rem var(--neon-text-edge-color), 0 0 8rem var(--neon-text-edge-color), -0.2rem -0.2rem 2rem var(--neon-text-shadow), 0.2rem 0.2rem 2rem var(--neon-text-shadow);
+    text-shadow: 0 0 1rem #fff, 0 0 2rem var(--neon-text-shadow), 0 0 3rem var(--neon-text-shadow), 0 0 4rem var(--neon-text-shadow);
   }
   0%, 21.999%, 63%, 63.999%, 65%, 69.999% {
     opacity: 0.4;
-    text-shadow: 0 0 1rem var(--neon-text-shadow), 0 0 2rem var(--neon-text-shadow), 0 0 4rem var(--neon-text-edge-color);
+    text-shadow: 0 0 1rem var(--neon-text-shadow), 0 0 2rem var(--neon-text-shadow);
   }
 `
 
